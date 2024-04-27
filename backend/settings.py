e37924f837e0ae83b987dee4434e57c7f6b9afb4
Oleg Shiryaev <oashiryaev@yandex.ru>
2024-04-27 17:34:00 +0300

    Modified settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -99,12 +99,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': '<dbname>',
+            'USER': 'postgres',
+            'PASSWORD': '<password>',
+            'HOST': 'localhost',
+            'PORT': 5432,
+        }
+    }
 
 CACHES = {
     'default': {
