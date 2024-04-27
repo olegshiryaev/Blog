6c41bfc9c35252d0a3f5fe396196486544dd691f
Oleg Shiryaev <oashiryaev@yandex.ru>
2024-04-27 17:37:05 +0300

    Modified settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(env('DEBUG', default=1))
+DEBUG = env('DEBUG') == 'True'
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS').split()
 
